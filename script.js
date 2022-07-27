3739d1428d3010ef984e193101d5137fdb52359d
Awais <awaischeema201@gmail.com>
2022-07-27 19:05:41 +0500

    btn and sliders

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const fileInput = document.querySelector('.file-input'),
+filterOptions = document.querySelectorAll('.filter button'),
+filterName = document.querySelector('.filter-info .name'),
+filterValue = document.querySelector('.filter-info .value'),
+filterSlider = document.querySelector('.slider input'),
 previewImg = document.querySelector('.preview-image img'),
 chooseImgBtn = document.querySelector('.choose-img');
+
+let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
 
 const loadImage = ()=>{
 let file = fileInput.files[0];
@@ -9,8 +15,46 @@ previewImg.src = URL.createObjectURL(file);
 previewImg.addEventListener('load',()=>{
     document.querySelector('.container').classList.remove('disable')
 })
+}
+filterOptions.forEach(option => {
+    option.addEventListener('click',()=>{
+        document.querySelector('.filter .active').classList.remove('active')
+        option.classList.add('active')
+        filterName.innerText = option.innerText
+        if(option.id === 'brightness'){
+            filterSlider.max = '200'
+            filterSlider.value = brightness
+            filterValue.innerText = `${brightness}%`
+        }else if(option.id === 'saturation'){
+            filterSlider.max = '200'
+            filterSlider.value = saturation
+            filterValue.innerText = `${saturation}%`
+        }else if(option.id === 'inversion'){
+            filterSlider.max = '100'
+            filterSlider.value = inversion
+            filterValue.innerText = `${inversion}%`
+        }else if(option.id === 'grayscale'){
+            filterSlider.max = '100'
+            filterSlider.value = grayscale
+            filterValue.innerText = `${grayscale}%`
+        }
+    })
+});
+const updateFilter = ()=>{
+filterValue.innerText = `${filterSlider.value}%`
+const selectedFilter = document.querySelector('.filter .active')
+if(selectedFilter.id === 'brightness'){
+    brightness = filterSlider.value
+}else if(selectedFilter.id === 'saturation'){
+    saturation = filterSlider.value
+}else if(selectedFilter.id === 'inversion'){
+    inversion = filterSlider.value
+}else if(selectedFilter.id === 'grayscale'){
+    grayscale = filterSlider.value
+}
 
 }
 
 fileInput.addEventListener('change',loadImage);
+filterSlider.addEventListener('input',updateFilter)
 chooseImgBtn.addEventListener('click',()=> fileInput.click());
