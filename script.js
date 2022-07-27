444f63c37be475c370386dc179379a15857b6721
Awais <awaischeema201@gmail.com>
2022-07-27 20:37:06 +0500

    finalize

--- script.js
+++ script.js
@@ -6,13 +6,14 @@ filterValue = document.querySelector('.filter-info .value'),
 filterSlider = document.querySelector('.slider input'),
 previewImg = document.querySelector('.preview-image img'),
 resetFilterBtn = document.querySelector('.reset-filter'),
-chooseImgBtn = document.querySelector('.choose-img');
+chooseImgBtn = document.querySelector('.choose-img'),
+saveImgBtn = document.querySelector('.save-img');
 
 let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
 let rotate = 0, filpHorizontal = 1,filpVertical = 1;
 const applyFilter = ()=>{
-    previewImg.style.transform = `rotate(${rotate}deg) scale(${filpHorizontal},${filpVertical})`
-    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`
+    previewImg.style.transform = `rotate(${rotate}deg) scale(${filpHorizontal},${filpVertical})`;
+    previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
 }
 
 const loadImage = ()=>{
@@ -20,6 +21,7 @@ let file = fileInput.files[0];
 if(!file) return;
 previewImg.src = URL.createObjectURL(file);
 previewImg.addEventListener('load',()=>{
+    resetFilterBtn.click();
     document.querySelector('.container').classList.remove('disable')
 })
 }
@@ -83,7 +85,25 @@ const resetFilters = ()=>{
     applyFilter();
 
 }
+const saveImage = ()=>{
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d');
+    canvas.width = previewImg.naturalWidth;
+    canvas.height = previewImg.naturalHeight;
+    ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) invert(${inversion}%) grayscale(${grayscale}%)`;
+    ctx.translate(canvas.width / 2,canvas.height / 2)
+    if(rotate !== 0){
+        ctx.rotate(rotate * Math.PI / 180)
+    }
+    ctx.scale(filpHorizontal,filpVertical)
+    ctx.drawImage(previewImg, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    let link = document.createElement('a');
+    link.download = 'image.jpg';
+    link.href = canvas.toDataURL();
+    link.click();
+}
 fileInput.addEventListener('change',loadImage);
-filterSlider.addEventListener('input',updateFilter)
-resetFilterBtn.addEventListener('click',resetFilters)
+filterSlider.addEventListener('input',updateFilter);
+resetFilterBtn.addEventListener('click',resetFilters);
+saveImgBtn.addEventListener('click',saveImage);
 chooseImgBtn.addEventListener('click',()=> fileInput.click());
